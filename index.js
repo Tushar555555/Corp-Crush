9c5557e448de23fa7bd0b02b3d2bf626ab88704e
Tushar555555 <gpt.tushar555@gmail.com>
2021-06-14 22:31:31 +0530

    add wordpress page and change navbar

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
-const $dropdown = $(".dropdown");
-const $dropdownToggle = $(".dropdown-toggle");
-const $dropdownMenu = $(".dropdown-menu");
-const showClass = "show";
+// const $dropdown = $(".dropdown");
+// const $dropdownToggle = $(".dropdown-toggle");
+// const $dropdownMenu = $(".dropdown-menu");
+// const showClass = "show";
 
-$(window).on("load resize", function() {
-  if (this.matchMedia("(min-width: 768px)").matches) {
-    $dropdown.hover(
-      function() {
-        const $this = $(this);
-        $this.addClass(showClass);
-        $this.find($dropdownToggle).attr("aria-expanded", "true");
-        $this.find($dropdownMenu).addClass(showClass);
-      },
-      function() {
-        const $this = $(this);
-        $this.removeClass(showClass);
-        $this.find($dropdownToggle).attr("aria-expanded", "false");
-        $this.find($dropdownMenu).removeClass(showClass);
-      }
-    );
-  } else {
-    $dropdown.off("mouseenter mouseleave");
-  }
-});
+// $(window).on("load resize", function() {
+//   if (this.matchMedia("(min-width: 768px)").matches) {
+//     $dropdown.hover(
+//       function() {
+//         const $this = $(this);
+//         $this.addClass(showClass);
+//         $this.find($dropdownToggle).attr("aria-expanded", "true");
+//         $this.find($dropdownMenu).addClass(showClass);
+//       },
+//       function() {
+//         const $this = $(this);
+//         $this.removeClass(showClass);
+//         $this.find($dropdownToggle).attr("aria-expanded", "false");
+//         $this.find($dropdownMenu).removeClass(showClass);
+//       }
+//     );
+//   } else {
+//     $dropdown.off("mouseenter mouseleave");
+//   }
+// });
 
+
+//fade
 $(window).on("load",function() {
   $(window).scroll(function() {
     var windowBottom = $(this).scrollTop() + $(this).innerHeight();
@@ -55,3 +57,54 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
+//fade ends
+
+
+//navbar start
+$(document).ready(function(){
+  $('.navbar-toggler').click(function(){
+      $('.navbar-collapse').slideToggle(300);
+  });
+  
+  smallScreenMenu();
+  let temp;
+  function resizeEnd(){
+      smallScreenMenu();
+  }
+
+  $(window).resize(function(){
+      clearTimeout(temp);
+      temp = setTimeout(resizeEnd, 100);
+      resetMenu();
+  });
+});
+
+
+const subMenus = $('.sub-menu');
+const menuLinks = $('.menu-link');
+
+function smallScreenMenu(){
+  if($(window).innerWidth() <= 992){
+      menuLinks.each(function(item){
+          $(this).click(function(){
+              $(this).next().slideToggle();
+          });
+      });
+  } else {
+      menuLinks.each(function(item){
+          $(this).off('click');
+      });
+  }
+}
+
+function resetMenu(){
+  if($(window).innerWidth() > 992){
+      subMenus.each(function(item){
+          $(this).css('display', 'none');
+      });
+  }
+}
+
+//navbar ends
+
+
